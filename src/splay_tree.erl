348335128afee2f8cd2210b5ec2588d3ce5130f4
Takeru Ohta <phjgt308@gmail.com>
2012-02-22 01:21:41 +0900

    store: zig-zig, zig-zag

--- src/splay_tree.erl
+++ src/splay_tree.erl
@@ -43,13 +43,32 @@ splay_node(Key, Node, Type, Path) ->
 
 splay([{_, X}]) -> X;
 splay([Xi, {_, P}]) ->
-    %% zip
+    %% zig
     case Xi of
         {left,  X} -> X#node{right=P#node{left=X#node.right}};
         {right, X} -> X#node{left=P#node{right=X#node.left}}
     end;
-splay(_=Path) ->
-    Path.
-            
-    
-     
+splay([{Type,X}, {Type,P}, {Type2,G}|Path]) ->
+    %% zig-zig
+    NewG = 
+        case Type of
+            left ->
+                X#node{right=P#node{right=G#node{left=P#node.right},
+                                    left=X#node.right}};
+            right ->
+                X#node{left=P#node{left=G#node{right=P#node.left},
+                                   right=X#node.left}}
+        end,
+    splay([{Type2,NewG}|Path]);
+splay([{Type,X}, {_,P}, {Type2,G}|Path]) ->
+    %% zig-zag
+    NewG = 
+        case Type of
+            left ->
+                X#node{right=P#node{left=X#node.right},
+                       left=G#node{right=X#node.left}};
+            right ->
+                X#node{left=P#node{right=X#node.left},
+                       right=G#node{left=X#node.right}}
+        end,
+    splay([{Type2,NewG}|Path]).
