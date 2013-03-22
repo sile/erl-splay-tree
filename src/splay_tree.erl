78c69b142273647842931db01160a9e12e37dd7e
Takeru Ohta <phjgt308@gmail.com>
2013-03-23 02:01:24 +0900

    型定義を追加

--- src/splay_tree.erl
+++ src/splay_tree.erl
@@ -1,40 +1,65 @@
 -module(splay_tree).
+-compile(inline).
 
 -export([new/0, store/3, find/2, erase/2, 
          update/4, filter/2, size/1, map/2,
          fold/3, from_list/1, to_list/1]).
 
-%%% records
--record(tree, {root=nil,
-               size=0}).
+-export_type([tree/0, key/0, value/0, 
+              update_fn/0, map_fn/0, fold_fn/0, pred_fn/0]).
 
--record(node, {key,
-               val,
-               lft=nil,
-               rgt=nil}).
+
+%%% records
+-record(tree, {root=nil :: maybe_tree_node(),
+               size=0   :: non_neg_integer()}).
+
+-record(node, {key     :: key(),
+               val     :: value(),
+               lft=nil :: maybe_tree_node(),
+               rgt=nil :: maybe_tree_node()}).
+
+%%% types
+-opaque tree()    :: #tree{}.
+-type key()       :: any().
+-type value()     :: any().
+-type update_fn() :: fun((value()) -> value()).
+-type map_fn()    :: fun((key(),value()) -> value()).
+-type fold_fn()   :: fun((key(),value(),term()) -> term()).
+-type pred_fn()   :: fun((key(),value()) -> boolean()).
+
+-type maybe_tree_node() :: tree_node()|nil.
+-type tree_node()       :: #node{}.
+-type direction()       :: lft|rgt.
 
 %%% exported functions
+-spec new() -> tree().
 new() -> #tree{}.
+
+-spec size(tree()) -> non_neg_integer().
 size(#tree{size=Size}) -> Size.
 
+-spec store(key(), value(), tree()) -> tree().
 store(Key, Value, #tree{root=Root, size=Size}) ->
     case path_to_node(Key, Root) of
         {nil,  Path} -> #tree{size=Size+1, root=splay(leaf(Key,Value), Path)};
         {Node, Path} -> #tree{size=Size,   root=splay(val(Node,Value), Path)}
     end.
 
+-spec update(key(), update_fn(), value(), tree()) -> tree().
 update(Key, Fun, Initial, #tree{root=Root, size=Size}) ->
     case path_to_node(Key, Root) of
         {nil,  Path} -> #tree{size=Size+1, root=splay(leaf(Key,Initial), Path)};
         {Node, Path} -> #tree{size=Size,   root=splay(val(Node,Fun(Node#node.val)), Path)}
     end.
 
+-spec find(key(), tree()) -> {error,tree()} | {{ok,value()},tree()}.
 find(Key, Tree=#tree{root=Root}) ->
     case path_to_node(Key,Root) of
         {nil,  Path} -> {error,              Tree#tree{root=splay(Path)}};
         {Node, Path} -> {{ok,Node#node.val}, Tree#tree{root=splay(Node,Path)}}
     end.
 
+-spec erase(key(), tree()) -> tree().
 erase(Key, #tree{root=Root, size=Size}) ->
     case path_to_node(Key, Root) of
         {nil,  Path} -> #tree{size=Size,   root=splay(Path)};
@@ -46,28 +71,42 @@ erase(Key, #tree{root=Root, size=Size}) ->
                                     end}
     end.
 
+-spec to_list(tree()) -> [{key(),value()}].
 to_list(Tree) -> lists:reverse(fold(fun (K, V, Acc) -> [{K,V}|Acc] end, [], Tree)).
+
+-spec from_list([{key(),value()}]) -> tree().
 from_list(List) -> lists:foldl(fun ({K, V}, Tree) -> store(K, V, Tree) end, new(), List).
-    
+
+-spec map(map_fn(), tree()) -> tree().
 map(Fun, Tree) -> Tree#tree{root=map_node(Fun, Tree#tree.root)}.
+
+-spec fold(fold_fn(), term(), tree()) -> term().
 fold(Fun, Acc0, Tree) -> fold_node(Fun, Tree#tree.root, Acc0).
+
+-spec filter(pred_fn(), tree()) -> tree().
 filter(Pred, Tree) -> Tree#tree{root=filter_node(Pred, Tree#tree.root)}.
 
 %%% auxiliary functions
+-spec leaf(key(), value()) -> tree_node().
 leaf(Key, Value) -> #node{key=Key, val=Value}.
+
+-spec val(tree_node(), value()) -> tree_node().
 val(Node, Value) -> Node#node{val=Value}.
 
+-spec pop_front(tree_node()) -> maybe_tree_node().
 pop_front(Node) ->
     case move_largest_node_to_front(Node#node.lft) of
         nil   -> Node#node.rgt;
         Front -> Front#node{rgt=Node#node.rgt}
     end.
 
+-spec move_largest_node_to_front(maybe_tree_node()) -> maybe_tree_node().
 move_largest_node_to_front(nil) ->
     nil;
 move_largest_node_to_front(Node) ->
     move_largest_node_to_front(Node, []).
 
+-spec move_largest_node_to_front(tree_node(), [tree_node()]) -> tree_node().
 move_largest_node_to_front(Node=#node{rgt=nil}, Path) ->
     Front = lists:foldl(fun (N, Front) -> N#node{rgt=Front} end,
                         Node#node.lft,
@@ -76,9 +115,12 @@ move_largest_node_to_front(Node=#node{rgt=nil}, Path) ->
 move_largest_node_to_front(Node, Path) ->
     move_largest_node_to_front(Node#node.rgt, [Node|Path]).
 
+-spec path_to_node(key(), maybe_tree_node()) -> {maybe_tree_node(), [{direction(),tree_node()}]}.
 path_to_node(Key, Root) -> 
     path_to_node(Key, Root, []).
 
+-spec path_to_node(key(), maybe_tree_node(), [{direction(),tree_node()}]) ->
+                          {maybe_tree_node(), [{direction(),tree_node()}]}.
 path_to_node(Key, Node, Path) ->
     case Node of
         nil                                     -> {nil,  Path};
@@ -87,9 +129,11 @@ path_to_node(Key, Node, Path) ->
         #node{key=Key}                          -> {Node, Path}
     end.
 
+-spec splay([{direction(),tree_node()}]) -> maybe_tree_node().
 splay([])              -> nil;
 splay([{_,Node}|Path]) -> splay(Node, Path).
 
+-spec splay(tree_node(), [{direction(),tree_node()}]) -> tree_node().
 splay(X, []) -> X;
 splay(X, [{Dir, P}]) ->                % zig
     case Dir of
@@ -109,16 +153,19 @@ splay(X, [{Dir,P}, {_,G} | Path]) ->   % zig-zag
           end,
           Path).
 
+-spec fold_node(fold_fn(), maybe_tree_node(), term()) -> term().
 fold_node(_, nil, Acc) ->
     Acc;
 fold_node(Fun, #node{key=Key, val=Value, lft=Lft, rgt=Rgt}, Acc) ->
     fold_node(Fun, Rgt, Fun(Key, Value, fold_node(Fun, Lft, Acc))).
 
+-spec map_node(map_fn(), maybe_tree_node()) -> maybe_tree_node().
 map_node(_, nil) ->
     nil;
 map_node(Fun, #node{key=Key, val=Value, lft=Lft, rgt=Rgt}) ->
     #node{key=Key, val=Fun(Key,Value), lft=map_node(Fun,Lft), rgt=map_node(Fun,Rgt)}.
 
+-spec filter_node(pred_fn(), maybe_tree_node()) -> maybe_tree_node().
 filter_node(_, nil) ->
     nil;
 filter_node(Pred, Node=#node{key=Key, val=Value, lft=Lft, rgt=Rgt}) ->
